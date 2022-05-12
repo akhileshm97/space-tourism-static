ff652e92688083951b45578f1ef55d6eb38da01e
Akhilesh Manuvarthi <akhileshmanuvarthi@gmail.com>
2022-05-12 23:50:36 +0530

    remove js code, add font

--- index.js
+++ index.js
@@ -15,26 +15,3 @@ window.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 });
-
-// const buttons = document.querySelectorAll('button');
-// const tabs = document.querySelectorAll('[role="tabpanel"]');
-// buttons.forEach((button, i) =>
-// 	button.addEventListener('click', (e) => changeTab(e, i))
-// );
-
-// function changeTab(e, i) {
-// 	console.log(e.target);
-// 	console.log(i);
-// 	tabs.forEach((tab) => tab.setAttribute('aria-selected', false));
-// 	tabs[i].setAttribute('aria-selected', true);
-// }
-// // fetch('/data.json').then(response => console.log(response.json()));
-// const message = document.querySelector('#message');
-
-// async function getData() {
-// 	return fetch('/data.json').then((response) => response.json());
-// }
-// const data = await getData();
-// console.log(data);
-// console.log(message);
-// message.textContent = data.crew.length;
